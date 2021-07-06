2294692bd2a3ab60516351a41a7bca83246d754a
raghav6202 <raghavchandravs@gmail.com>
2021-07-06 22:12:22 +0530

    feat: minion trans

--- app.js
+++ app.js
@@ -8,14 +8,39 @@ btnTranslate.addEventListener("click", clickEventHandler);
 InputTxt.addEventListener("click",clickEventHandler);
 // addEventListener----->>>  https://www.w3schools.com/js/js_htmldom_eventlistener.asp
 
+// var serverURL= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTransaltionalURL(text)
+{
+    return serverURL + "?" + "text= " + text;
+}
+
+
+function errorHandler(error)
+{
+    console.log("error occured");
+    alert("There are certain server issues PLEASE TRY AGAIN LATER");
+
+}  //display part of error handling
+
 function clickEventHandler()
 {
     // console.log("clicked");
     // console.log("Input",InputTxt.value)
-
      //InputTxt.value reads the input
 
-    OutputDisplay.innerText = "banana speak  " + InputTxt.value;
+//   OutputDisplay.innerText = "banana text " + InputTxt.value;
+// The innerText property prints or returns the text content of the specified node, and all its descendants
+var inputText = InputTxt.value;
 
-    // The innerText property prints or returns the text content of the specified node, and all its descendants
-}
+fetch(getTransaltionalURL(inputText))
+.then(response => response.json())
+.then(json => {
+
+    var translatedText = json.contents.translated;
+    OutputDisplay.innerText = translatedText;
+})
+  .catch(errorHandler) 
+};
